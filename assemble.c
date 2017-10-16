9a138af99664b421f154431ba0de8352d23500e4
lurunhe <lurunhe@umich.edu>
2017-10-15 20:59:21 -0400

    P2

--- assemble.c
+++ assemble.c
@@ -6,6 +6,16 @@
 //123
 #define MAXLINELENGTH 1000
 
+struct symbol{
+	char* name;
+	char type;
+	int line;
+}symbol;
+struct relocation{
+	char* opcode;
+	char* name;
+	int line;
+}relocation;
 int readAndParse(FILE *, char *, char *, char *, char *, char *);
 int isNumber(char *);
 int
@@ -24,7 +34,8 @@ main(int argc, char *argv[])
     FILE *inFilePtr, *outFilePtr;
     char label[MAXLINELENGTH], opcode[MAXLINELENGTH], arg0[MAXLINELENGTH],
             arg1[MAXLINELENGTH], arg2[MAXLINELENGTH];
-	int bnum,offsetnum;
+	int offsetnum;
+	int bnum[1000];
 	char* labelindex[1000]={0};
 	int j;
 	for(j=0;j<1000;j++){
@@ -54,6 +65,26 @@ main(int argc, char *argv[])
     /* here is an example for how to use readAndParse to read a line from
         inFilePtr */
 	int i=0;
+	int data[1000];
+	int datanum=0;
+//	printf("1");
+	struct symbol symbolTable[1000];
+	struct relocation relocationTable[1000];
+	for(j=0;j<1000;++j){
+		//printf("1");
+		symbolTable[j].name=(char*)malloc(1000*sizeof(char));
+
+		symbolTable[j].type='U';
+		symbolTable[j].line=0;
+		relocationTable[j].name=(char*)malloc(1000*sizeof(char));
+
+		relocationTable[j].opcode=(char*)malloc(1000*sizeof(char));
+
+		relocationTable[j].line=0;
+	}
+	int symbolnum=0;
+	int linenum1=0;
+	int linenum2=0;
     while(readAndParse(inFilePtr, label, opcode, arg0, arg1, arg2)!=0) {
         /* reached end of file */
 		if(strcmp(label,"") && (label[0]=='0' || label[0]=='1' || label[0]=='2' || label[0]=='3' || label[0]=='4' || label[0]=='5' || label[0]=='6' || label[0]=='7' || label[0]=='8' || label[0]=='9' || strlen(label)>6)){
@@ -61,7 +92,28 @@ main(int argc, char *argv[])
 			exit(1);		
 		}
 		strcpy(labelindex[i],label);
+		if(label[0]>='A' && label[0]<='Z'){
+			if(!strcmp(opcode,".fill")){
+				//printf("1");
+				strcpy(symbolTable[symbolnum].name,label);
+				symbolTable[symbolnum].line=linenum2;
+				symbolTable[symbolnum].type='D';
+				//printf("%s %s %d",label,symbolTable[symbolnum].name,symbolnum);
+				symbolnum++;	
+			}
+			else{
+				strcpy(symbolTable[symbolnum].name,label);
+				symbolTable[symbolnum].line=linenum1;
+				symbolTable[symbolnum].type='T';
+				symbolnum++;
+			}
+		}
+		if(!strcmp(opcode,".fill"))
+			linenum2++;
+		else
+			linenum1++;
 //		printf("%s\n",labelindex[0]);
+		
 		i++;
     }
 //	printf("%s\n",labelindex[2]);
@@ -82,17 +134,25 @@ main(int argc, char *argv[])
         opcode */
 		int PC=0;
 		int flag;
+		int textnum=0;
+	//	int j;
+		int ifsymbol;
+		int rnum=0;
 	while(readAndParse(inFilePtr, label, opcode, arg0, arg1, arg2)!=0){ 
-			if (!strcmp(opcode, "add")) {
+		flag=0;
+		ifsymbol=0;
+		if (!strcmp(opcode, "add")) {
    		/* do whatever you need to do for opcode "add" */
-			bnum=(atoi(arg0)<<19)+(atoi(arg1)<<16)+atoi(arg2);
+			bnum[textnum]=(atoi(arg0)<<19)+(atoi(arg1)<<16)+atoi(arg2);
 			//num=convertNum(bnum);
 			//printf("%d\n",bnum);
-			fprintf(outFilePtr,"%d\n",bnum);
+		//	fprintf(outFilePtr,"%d\n",bnum);
+			textnum++;
 		}
 		else if(!strcmp(opcode,"nor")){
-			bnum=(1<<22)+(atoi(arg0)<<19)+(atoi(arg1)<<16)+atoi(arg2);
-			fprintf(outFilePtr,"%d\n",bnum);
+			bnum[textnum]=(1<<22)+(atoi(arg0)<<19)+(atoi(arg1)<<16)+atoi(arg2);
+			textnum++;
+		//	fprintf(outFilePtr,"%d\n",bnum);
 		}
 		else if(!strcmp(opcode,"lw")){
 			if(isNumber(arg2)==1){
@@ -102,31 +162,56 @@ main(int argc, char *argv[])
 					exit(1);
 				}
 				if(offsetnum>=0)
-					bnum=(1<<23)+(atoi(arg0)<<19)+(atoi(arg1)<<16)+offsetnum;
+					bnum[textnum]=(1<<23)+(atoi(arg0)<<19)+(atoi(arg1)<<16)+offsetnum;
 				else
-					bnum=(1<<23)+(atoi(arg0)<<19)+(atoi(arg1)<<16)+(unsigned int)offsetnum-(65535<<16);
-
-				fprintf(outFilePtr,"%d\n",bnum);
+					bnum[textnum]=(1<<23)+(atoi(arg0)<<19)+(atoi(arg1)<<16)+(unsigned int)offsetnum-(65535<<16);
+				textnum++;
+				//fprintf(outFilePtr,"%d\n",bnum);
 			}
 			else{
+				relocationTable[rnum].opcode="lw";
+				strcpy(relocationTable[rnum].name,arg2);
+				relocationTable[rnum].line=textnum;
+				rnum++;
+
 				for(i=0;i<1000;i++){
 					if(!strcmp(labelindex[i],arg2)){
+						//printf("%d",isNumber("SubAdr"));
+					
 						offsetnum=i;
 						flag=1;
 						break;
 					}
 				}
 				if(flag!=1){
-					printf("error: undefined labels\n");
-					exit(1);
+					if(arg2[0]>='A' && arg2[0]<='Z'){
+						for(j=0;j<1000;++j){
+							if(!strcmp(symbolTable[j].name,arg2)){
+								ifsymbol=1;
+								break;
+							}
+						}
+							if(ifsymbol==0){
+								strcpy(symbolTable[symbolnum].name,arg2);
+								symbolTable[symbolnum].type='U';
+								symbolTable[symbolnum].line=0;
+								symbolnum++;
+							}
+						offsetnum=0;
+					}
+					else{
+						printf("error: undefined local labels\n");
+						exit(1);
+					}
 				}
 				if(offsetnum>=((1<<15)) || offsetnum<(-(1<<15))){
 					printf("error: offset number do not fit in 16 bits\n");
 					exit(1);
 				}
 
-				bnum=(1<<23)+(atoi(arg0)<<19)+(atoi(arg1)<<16)+offsetnum;
-				fprintf(outFilePtr,"%d\n",bnum);	
+				bnum[textnum]=(1<<23)+(atoi(arg0)<<19)+(atoi(arg1)<<16)+offsetnum;
+				textnum++;
+			//	fprintf(outFilePtr,"%d\n",bnum);	
 			}		
 		}
 		else if(!strcmp(opcode,"sw")){
@@ -138,23 +223,46 @@ main(int argc, char *argv[])
 					exit(1);
 				}
 				if(offsetnum>=0)
-					bnum=(1<<22)+(1<<23)+(atoi(arg0)<<19)+(atoi(arg1)<<16)+offsetnum;
+					bnum[textnum]=(1<<22)+(1<<23)+(atoi(arg0)<<19)+(atoi(arg1)<<16)+offsetnum;
 				else
-					bnum=(3<<22)+(atoi(arg0)<<19)+(atoi(arg1)<<16)+(unsigned int)offsetnum-(65535<<16);
-
-				fprintf(outFilePtr,"%d\n",bnum);
+					bnum[textnum]=(3<<22)+(atoi(arg0)<<19)+(atoi(arg1)<<16)+(unsigned int)offsetnum-(65535<<16);
+				textnum++;
+			//	fprintf(outFilePtr,"%d\n",bnum);
 			}
 			else{
+				relocationTable[rnum].opcode="sw";
+				strcpy(relocationTable[rnum].name,arg2);
+				relocationTable[rnum].line=textnum;
+				rnum++;
 				for(i=0;i<1000;i++){
 					if(!strcmp(labelindex[i],arg2)){
 						offsetnum=i;
 						flag=1;
 						break;
-					}		
+					}
 				}
 				if(flag!=1){
-					printf("error: undefined labels\n");
-					exit(1);
+
+					if(arg2[0]>='A' && arg2[0]<='Z'){
+						offsetnum=0;
+						for(j=0;j<1000;++j){
+							if(!strcmp(symbolTable[j].name,arg2)){
+								ifsymbol=1;
+								break;
+							}
+						}
+							if(ifsymbol==0){
+								//printf("%s\n",symbolTable[0].name);
+								strcpy(symbolTable[symbolnum].name,arg2);
+								symbolTable[symbolnum].type='U';
+								symbolTable[symbolnum].line=0;
+								symbolnum++;
+							}
+					}
+					else{
+						printf("error: undefined labels\n");
+						exit(1);
+					}
 				}
 
 				if(offsetnum>=((1<<15)) || offsetnum<(-(1<<15))){
@@ -162,25 +270,30 @@ main(int argc, char *argv[])
 					exit(1);
 				}
 
-				bnum=(3<<22)+(atoi(arg0)<<19)+(atoi(arg1)<<16)+offsetnum;
-				fprintf(outFilePtr,"%d\n",bnum);	
+				bnum[textnum]=(3<<22)+(atoi(arg0)<<19)+(atoi(arg1)<<16)+offsetnum;
+				//fprintf(outFilePtr,"%d\n",bnum);
+				textnum++;
 			}
 		}
 		else if(!strcmp(opcode,"beq")){
 				if(isNumber(arg2)==1){
-						offsetnum=atoi(arg2);
-				if(offsetnum>=((1<<15)) || offsetnum<(-(1<<15))){
-					printf("error: offset number do not fit in 16 bits\n");
-					exit(1);
-				}
-				if(offsetnum>=0)
-					bnum=(4<<22)+(atoi(arg0)<<19)+(atoi(arg1)<<16)+offsetnum;
-				else
-					bnum=(4<<22)+(atoi(arg0)<<19)+(atoi(arg1)<<16)+(unsigned int)offsetnum-(65535<<16);
-
-				fprintf(outFilePtr,"%d\n",bnum);
+					offsetnum=atoi(arg2);
+					if(offsetnum>=((1<<15)) || offsetnum<(-(1<<15))){
+						printf("error: offset number do not fit in 16 bits\n");
+						exit(1);
+					}
+					if(offsetnum>=0)
+						bnum[textnum]=(4<<22)+(atoi(arg0)<<19)+(atoi(arg1)<<16)+offsetnum;
+					else
+						bnum[textnum]=(4<<22)+(atoi(arg0)<<19)+(atoi(arg1)<<16)+(unsigned int)offsetnum-(65535<<16);	
+					textnum++;
+			//	fprintf(outFilePtr,"%d\n",bnum);
 				}
 				else{
+				//	relocationTable[rnum].opcode="beq";
+				//	relocationTable[rnum].name=arg2;
+				//	relocationTable[rnum].line=textnum;
+				//	rnum++;
 						//printf("%s\n",labelindex[2]);
 						for(i=0;i<1000;i++){
 							if(!strcmp(labelindex[i],arg2)){
@@ -199,49 +312,69 @@ main(int argc, char *argv[])
 						}
 		
 						if(offsetnum>=0)
-							bnum=(4<<22)+(atoi(arg0)<<19)+(atoi(arg1)<<16)+offsetnum;
+							bnum[textnum]=(4<<22)+(atoi(arg0)<<19)+(atoi(arg1)<<16)+offsetnum;
 						else
-							bnum=(4<<22)+(atoi(arg0)<<19)+(atoi(arg1)<<16)+(unsigned int)offsetnum-(65535<<16);
-						fprintf(outFilePtr,"%d\n",bnum);
+							bnum[textnum]=(4<<22)+(atoi(arg0)<<19)+(atoi(arg1)<<16)+(unsigned int)offsetnum-(65535<<16);
+						textnum++;
+					//	fprintf(outFilePtr,"%d\n",bnum);
 				}
 		}
 		else if(!strcmp(opcode,"jalr")){
-			bnum=(5<<22)+(atoi(arg0)<<19)+(atoi(arg1)<<16);
-			fprintf(outFilePtr,"%d\n",bnum);
+			bnum[textnum]=(5<<22)+(atoi(arg0)<<19)+(atoi(arg1)<<16);
+			textnum++;
+		//	fprintf(outFilePtr,"%d\n",bnum);
 		}
 		else if(!strcmp(opcode,"halt")){
-			bnum=(6<<22);
-			fprintf(outFilePtr,"%d\n",bnum);
+			bnum[textnum]=(6<<22);
+			textnum++;
+		//	fprintf(outFilePtr,"%d\n",bnum);
 		}
 		else if(!strcmp(opcode,"noop")){
-			bnum=(7<<22);
-			fprintf(outFilePtr,"%d\n",bnum);
+			bnum[textnum]=(7<<22);
+			textnum++;
+		//	fprintf(outFilePtr,"%d\n",bnum);
 		}
 		else if(!strcmp(opcode,".fill")){
 			if(isNumber(arg0)==1){
-				bnum=atoi(arg0);
-				fprintf(outFilePtr,"%d\n",bnum);
+				data[datanum]=atoi(arg0);
+				datanum++;
+			//	fprintf(outFilePtr,"%d\n",bnum);
 			}
 			else{
+				relocationTable[rnum].opcode=".fill";
+				strcpy(relocationTable[rnum].name,arg0);
+				relocationTable[rnum].line=datanum;
+				rnum++;
 				for(i=0;i<1000;i++){
 					if(!strcmp(labelindex[i],arg0)){	
-						bnum=i;
+						data[datanum]=i;
+						datanum++;
 						break;
 					}
 				}
-				fprintf(outFilePtr,"%d\n",bnum);
+			//	fprintf(outFilePtr,"%d\n",bnum);
 			}
 		}
 		else{
 			printf("error: unrecognized opcodes.\n");
 			exit(1);
 		}
+	//	datanum++;
+	//	textnum++;
+	//	symbolnum++;
 		PC++;	
 	}
-	
-
-    
-		
+	fprintf(outFilePtr,"%d %d %d %d\n",textnum,datanum,symbolnum,rnum);
+//	if(linenum1!=textnum || linenum2!=textnum)
+//		printf("wrong");
+	for(i=0;i<textnum;++i)
+		fprintf(outFilePtr,"%d\n",bnum[i]);
+	for(i=0;i<datanum;++i)
+		fprintf(outFilePtr,"%d\n",data[i]);
+    for(i=0;i<symbolnum;++i)
+		fprintf(outFilePtr,"%s %c %d\n",symbolTable[i].name,symbolTable[i].type,symbolTable[i].line);
+	for(i=0;i<rnum;++i)
+		fprintf(outFilePtr,"%d %s %s\n",relocationTable[i].line,relocationTable[i].opcode,relocationTable[i].name);
     return(0);
 }
 
---
+++ test4.as
@@ -0,0 +1,6 @@
+subOne	lw	0	2	neg1
+	add	1	2	1    
+	jalr	7	6
+	beq	7	7	Local
+neg1	.fill  	-1
+SubAdr	.fill	subOne
